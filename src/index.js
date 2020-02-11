e2480496213142d049975b6a12e47bc5a358c770
Casey Fayard <fayardcasey@gmail.com>
2020-02-11 13:55:29 -0500

    bonus-exercise-redux-todo-app, pushing original code

--- src/index.js
+++ src/index.js
@@ -1,61 +1,26 @@
-// {
-//     contact: 'James Smith'
-// }
-// {
-//     type: ADD_CONTACT,
-//     payload: {
-//         company: 'Professional Boxer',
-//         name: 'Evander Hollyfield',
-//         phoneNumber: '770-913-2267',
-//         email: 'evanderH@gmail.com',
-//         placeMet: 'Turner Field',
-//         reachedOutSecondTime: 'True',
-//         isCompanyHiring: 'True',
-//         haveSubmittedResume: 'True'
-
-//     }
-// }
-
-// {
-//     jobs: 'Abelton'
-// }
-// {
-//     type: ADD_JOB,
-//     payload: {
-//         company: 'Abelton Live',
-//         contactPerson: 'Scott Storch',
-//         phoneNumber: '990-913-2267',
-//         email: 'abletonhq@live.com',
-//         timesInterviewed: '1',
-//         interviewType: 'in-person',
-//         sentThankyYouNote: 'True',
-//         isCompanyHiring: 'True',
-//         position: 'Software Sales',
-//         haveSubmittedResume: 'True',
-//         levelOfHopeToGet: 10
-//     }
-// }
-
-// {
-//     type: isFormVisible,
-//     payload: {
-//         isFormVisible: true,
-//     }
-// }
 
 import { 
     createStore,
     combineReducers
 } from 'redux';
 
-const ADD_CONTACT = 'ADD_CONTACT'
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_COMPLETED = 'TOGGLE_COMPLETED'
 
-const defaultContactsState = { contacts: ''} ;
-function contacts(state=defaultContactsState, action) {
+const defaultTodosState = { todos: ''};
+
+function todos(state=defaultTodosState, action) {
     const newState = { ...state };
     switch (action.type) {
-        case ADD_CONTACT:
-            newState.contacts = action.payload;
+        case ADD_TODO:
+            newState.todos = action.payload;
+            break;
+        case REMOVE_TODO:
+            newState.todos = action.payload;
+            break;
+        case TOGGLE_COMPLETED:
+            newState.todos = action.payload;
             break;
         default:
             // console.log('im right here');
@@ -64,95 +29,47 @@ function contacts(state=defaultContactsState, action) {
     return newState;
 }
 
-function actionUpdateContact() {
+function actionAddTodo() {
     return {
-        type: ADD_CONTACT,
+        type: ADD_TODO,
         payload: {
-            company: 'Professional Boxer',
-            name: 'Evander Hollyfield',
-            phoneNumber: '770-913-2267',
-            email: 'evanderH@gmail.com',
-            placeMet: 'Turner Field ol-skool Braves meet-up',
-            reachedOutSecondTime: 'True',
-            isCompanyHiring: 'True',
-            haveSubmittedResume: 'True'
-    
+            todo1: 'aljfl;ajbblajlbaljbaljbaljb',  
+        }
+    }
+}
+function actionRemoveTodo() {
+    return {
+        type: REMOVE_TODO,
+        payload: {
+            todo1: '',  
+        }
+    }
+}
+function actionToggleCompleted() {
+    return {
+        type: TOGGLE_COMPLETED,
+        payload: {
+            complete: true,  
         }
     }
 }
 
 const ADD_JOB = 'ADD_JOB'
 
-const defaultJobsState = { jobs: ''} ;
-function jobs(state=defaultJobsState, action) {
-    const newState = { ...state };
-    switch (action.type) {
-        case ADD_JOB:
-            newState.jobs = action.payload;
-            break;
-        default:
-            // console.log('im right here jobsjobsaddjobspleasegetjob');
-            break;
-    }
-    return newState;
-}
 
-function actionUpdateJob() {
-    return {
-        type: ADD_JOB,
-        payload: {
-            company: 'Abelton Live',
-            contactPerson: 'Scott Storch',
-            phoneNumber: '990-913-2267',
-            email: 'abletonhq@live.com',
-            timesInterviewed: '1',
-            interviewType: 'in-person',
-            sentThankyYouNote: 'True',
-            isCompanyHiring: 'True',
-            position: 'Software Sales',
-            haveSubmittedResume: 'True',
-            levelOfHopeToGet: 10
-        }
-    }
-}
+// const rootReducer = combineReducers({
+//     // part of tree : reducer function
+//     contacts: contacts,
+//     jobs: jobs,
+//     forms: forms
+// });
 
-const IS_FORM_VISIBLE = 'IS_FORM_VISIBLE'
-
-const defaultFormState = { formVisible: false} ;
-function forms(state=defaultFormState, action) {
-    const newState = { ...state };
-    switch (action.type) {
-        case IS_FORM_VISIBLE:
-            newState.formVisible = action.payload;
-            break;
-        default:
-            // console.log('im right here jobsjobsaddjobspleasegetjob');
-            break;
-    }
-    return newState;
-}
-
-function actionFormVisibility() {
-    return {
-        type: IS_FORM_VISIBLE,
-        payload: {
-            formVisible: true,
-        }
-    }
-}
-
-const rootReducer = combineReducers({
-    // part of tree : reducer function
-    contacts: contacts,
-    jobs: jobs,
-    forms: forms
-});
-
-const store = createStore(  rootReducer,                             
+const store = createStore(  todos,                             
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 window.store = store;
-window.actionUpdateContact = actionUpdateContact;
+window.actionAddTodo = actionAddTodo;
+window.actionRemoveTodo = actionRemoveTodo;
 store.subscribe(() => {
 // just for debugging!
 console.table(store.getState());
